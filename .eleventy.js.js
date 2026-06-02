be6ae2fcc93586f49ad8fa4dd6d05ed239a6809a
nextfrontierfinancial <nextfrontierfinancial25@gmail.com>
2026-06-02 14:20:26 -0600

    Add files via upload

---
+++ .eleventy.js.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+  // Sort posts automatically so the newest is always on top
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
+  });
+
+  // Pass through your image folders straight to the finished site
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      layouts: "_includes/layouts"
+    }
+  };
+};
